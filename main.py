95c5194f90ea28dac5273fb2d163ea84b15587f6
dmeirLemas <demir2005elmas@hotmail.com>
2024-03-08 09:03:42 +0100

    first commit

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import newNN
 import os
 import csv
 import pandas as pd
+import gc
 
 if not os.path.exists("results_folder"):
-    # Create the folder
     os.makedirs("results_folder")
 
 folder_path = "snp500_data"
-
 files = os.listdir(folder_path)
 files.sort(reverse=True)
 
@@ -24,7 +23,7 @@ header = ["Company", "Percentage Change"]
 
 for file in files:
     print("-------------------------------------")
-    print("BEGINING " + file)
+    print("BEGINNING " + file)
     file_path = folder_path + "/" + file
     try:
         preproccessing.main(file_path)
@@ -45,10 +44,18 @@ for file in files:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(new_row)
 
+        out = Close = df["Close"][len(df["Close"]) - 120 :]
+        out.to_csv(f"prices/{file}")
+
+        # Explicitly collect and dump memory
+        gc.collect()
+        gc.garbage.clear()
+
+        os.remove(file_path)
+
     except Exception as e:
         if e == KeyboardInterrupt:
             exit()
         else:
-
             with open("BITCHES.txt", "a", encoding="utf-8") as f:
                 f.writelines(file_path + ": " + "\n" + str(e) + "\n")
